dd01b90747af7c73db187233a40ff4c90d2adf7e
lokeshvasnik <lokeshvasnik2003@gmail.com>
2023-02-05 19:51:27 +0530

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import frontEndSchema from './model/frontendSchema.js';
-import dotenv from 'dotenv';
-dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
